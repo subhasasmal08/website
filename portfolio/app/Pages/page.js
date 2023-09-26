5d5d24a6b25cb0a6ce059fed9743bae70252fb4c
subhasasmal08 <subhasasmal08@gmail.com>
2023-09-27 02:49:06 +0530

    home page started

--- portfolio/app/Pages/page.js
+++ portfolio/app/Pages/page.js
@@ -1,10 +1,11 @@
 "use client"
 import React, { Component } from 'react'
+import "../Pages/pages.scss"
 
 export default class page extends Component {
   render() {
     return (
-      <div>Pages</div>
+      <div className='pages_header_tag'>Pages</div>
     )
   }
 }
--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,9 +1,30 @@
+"use client";
+import React, { Component } from "react";
+import Image from "next/image";
+import hero from "../public/assets/Images/hero1.png";
+import "./home.scss";
 
-
-export default function Home() {
-  return (
-   <div>
-    Home page
-   </div>
-  )
+export default class page extends Component {
+  render() {
+    return (
+      <div className="home_page_main_wrapper">
+        <div className="partition_wrapper">
+          <div className="lhs_wrapper">
+            <h1 className="lhs_header">
+              Support your personal development and chase your dream
+            </h1>
+            <p className="lhs_subheader">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam
+            </p>
+            <button className="lhs_button">Get Started Today</button>
+          </div>
+          <div className="rhs_wrapper">
+            <Image src={hero} alt="hero" className="hero" placeholder="blur" />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
